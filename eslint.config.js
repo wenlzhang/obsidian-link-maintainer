837fcfe6accf5a141285c76cd1e8ed0039664833
wz <44930227+wenlzhang@users.noreply.github.com>
2024-12-07 16:52:05 +0100

    build: update esbuild config to use build API

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-useless-escape': 'off',
+      'no-var': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'no-prototype-builtins': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-this-alias': [
+        'error',
+        {
+          allowDestructuring: false,
+          allowedNames: ['self']
+        }
+      ]
+    }
+  }
+);
